fb81e5869941797e383678b2c93ef335dc5a4b43
santoadji21 <ahmad.aji.santoso21@gmail.com>
2022-04-28 18:33:53 +0700

    add css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css"
+import "./index.css";
+import "./tailwind.css";
 
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
